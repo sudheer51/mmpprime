e3361be3c017733d3081087f86f5d602669caee4
sudheer <sudheer694@gmail.com>
2023-06-16 07:41:03 +0530

    Added the precondition

--- mmpprime/src/test/java/org/mmp/patientmodule/tests/MMP_004_SendMessages.java
+++ mmpprime/src/test/java/org/mmp/patientmodule/tests/MMP_004_SendMessages.java
@@ -1,5 +1,23 @@
 package org.mmp.patientmodule.tests;
 
-public class MMP_004_SendMessages {
+import org.mmp.methods.BaseClass;
+import org.mmp.methods.HelperClass;
+import org.mmp.methods.MMPUtil;
+import org.mmp.patientmodule.pages.ScheduleAppointmentPage;
+import org.testng.annotations.BeforeMethod;
 
+public class MMP_004_SendMessages extends BaseClass{
+
+	MMPUtil mmpUtil ;
+	ScheduleAppointmentPage sPage;
+	
+	@BeforeMethod
+	public void preRequisite()
+	{
+		mmpUtil = new MMPUtil(driver);
+		mmpUtil.lauchBrowser(pro.getProperty("patientURL"));
+		HelperClass helperObj = new HelperClass(driver);
+		helperObj.login(pro.getProperty("patientUsername"), pro.getProperty("patientPassword"));
+		sPage = new ScheduleAppointmentPage(driver);
+	}
 }
